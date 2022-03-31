a96b7c8c1d84969f15a26ab780a5bf7890392467
long <17272827+longb1@users.noreply.github.com>
2022-03-31 19:46:32 +0100

    Create Typing Game
    Program typing game with Javscript and implement on page with CSS
    styling. Follow tutorial to improve my Javscript sckills

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('textDisplay')
 const quoteInputElement = document.getElementById('textInput')
+const timerElement = document.getElementById('timer')
+
+quoteInputElement.addEventListener('input', ()=>{
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+    let correct = true
+    arrayQuote.forEach((characterSpan, index)=>{
+        const character = arrayValue[index]
+        if(character==null){
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if(character===characterSpan.innerText){
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        }else{
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+
+    if(correct) renderNewQuote()
+})
 
 function getRandomQuote(){
     return fetch(RANDOM_QUOTE_API_URL)
@@ -10,8 +34,26 @@ function getRandomQuote(){
 
 async function renderNewQuote(){
     const QUOTE = await getRandomQuote()
-    quoteDisplayElement.innerText=QUOTE
+    quoteDisplayElement.innerHTML=''
+    QUOTE.split('').forEach(character =>{
+        const characterSpan = document.createElement('span')
+        characterSpan.innerText = character
+        quoteDisplayElement.appendChild(characterSpan)
+    })
     quoteInputElement.value = null
+    startTimer()
+}
+let startTime
+function startTimer(){
+    timerElement.innerText=0
+    startTime=new Date()
+    setInterval(() =>{
+        timer.innerText = getTimerTime()
+    },1000)
+}
+
+function getTimerTime(){
+   return Math.floor(new Date() - startTime)/1000
 }
 
 renderNewQuote()
