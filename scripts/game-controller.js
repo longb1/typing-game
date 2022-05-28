05b96787869c18c5469991c3d8c91ba0373e13ce
Long <17272827+longb1@users.noreply.github.com>
2022-05-28 11:46:17 +0100

    remove a function
    remove renderanewquote function, as it performs a similar role to game
    controller function, so I've effectively merged them into one.
    I've also delegated some jobs to the reset module.

--- scripts/game-controller.js
+++ scripts/game-controller.js
@@ -3,12 +3,13 @@ import splitByChar from "./split-by-char.js"
 import splitByWord from "./split-by-word.js"
 
 //controls flow of game from when user clikcs button to generating quote
-export default function controller(){
-    const newQuote= rendernewquote()
+export default async function controller(){
+    const QUOTE = await getRandomQuote()
     const wordArray = splitByWord(QUOTE)
     const charArray = splitByChar(QUOTE)
 
 
     inputField(wordArray)
-    return 
+    
+    return QUOTE
 }
--- scripts/text-complete.js
+++ scripts/text-complete.js
@@ -2,6 +2,8 @@ export default function reset(){
     arrayIndex = 0;
     currentWord='';
     wordArray=[];
+    quoteDisplayElement.innerHTML=''
+    quoteInputElement.value = null
     //textInput no pointer (blank it out cant access)
     //textdisplat empty
 }
