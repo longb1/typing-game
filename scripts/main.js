bec842bdd5c7d6fb2fcc29797d37e2e8b3056218
Long <17272827+longb1@users.noreply.github.com>
2022-05-28 12:18:04 +0100

    add global variables and eventlisteners
    add all global variables and eventlistneres to main file where it can be
    viewed, leave function modules in their own file.

--- scripts/main.js
+++ scripts/main.js
@@ -2,35 +2,52 @@ import move from "./animate-race.js"
 import splitByChar from "./split-by-char.js"
 import splitByWord from "./split-by-word.js"
 import inputField from "./input-field.js"
+import checkInput from "./input-field.js"
 
 const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random'
 const quoteDisplayElement = document.getElementById('textDisplay')
 const quoteInputElement = document.getElementById('textInput')
 const timerElement = document.getElementById('timer')
 
+let arrayIndex = 0;
+let currentWord;
+let wordArray;
 
-// quoteInputElement.addEventListener('input', ()=>{
-//     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
-//     const arrayValue = quoteInputElement.value.split('')
-//     let correct = true
-//     arrayQuote.forEach((characterSpan, index)=>{
-//         const character = arrayValue[index]
-//         if(character==null){
-//             characterSpan.classList.remove('correct')
-//             characterSpan.classList.remove('incorrect')
-//             correct = false
-//         } else if(character===characterSpan.innerText){
-//             characterSpan.classList.add('correct')
-//             characterSpan.classList.remove('incorrect')
-//         }else{
-//             characterSpan.classList.remove('correct')
-//             characterSpan.classList.add('incorrect')
-//             correct = false
-//         }
-//     })
+const textInput = document.getElementById('textInput')
 
-//     if(correct) renderNewQuote()
-// })
+textInput.addEventListener('keypress',function(e){
+    if (e.key==" "){
+        //get value
+        checkInput(textInput.value)
+    }
+})
+
+
+const startBtn = document.getElementById('raceBtn')
+startBtn.addEventListener('click',start())
+
+quoteInputElement.addEventListener('input', ()=>{
+    const arrayQuote = quoteDisplayElement.querySelectorAll('span')
+    const arrayValue = quoteInputElement.value.split('')
+    let correct = true
+    arrayQuote.forEach((characterSpan, index)=>{
+        const character = arrayValue[index]
+        if(character==null){
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if(character===characterSpan.innerText){
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        }else{
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+
+    if(correct) renderNewQuote()
+})
 
 function getRandomQuote(){
     return fetch(RANDOM_QUOTE_API_URL)
@@ -38,18 +55,7 @@ function getRandomQuote(){
         .then(data => data.content)
 }
 
-async function renderNewQuote(){
-    quoteDisplayElement.innerHTML=''
-    const QUOTE = await getRandomQuote()
-    
-    startTimer()
-    quoteInputElement.value = null
-    return QUOTE
-}
-
-const currentQuote = renderNewQuote;
-
-
+//timers
 let startTime
 function startTimer(){
     timerElement.innerText=0
@@ -62,5 +68,3 @@ function startTimer(){
 function getTimerTime(){
    return Math.floor(new Date() - startTime)/1000
 }
-
-renderNewQuote()
