4080caf303d0c63c74aeb5fb1242bc9eab90aa26
Long <17272827+longb1@users.noreply.github.com>
2022-05-28 08:20:21 +0100

    rename main.js and define type
    rename main.js for better clarification, and also define type as
    module so i can import modules from different javascript files, allowing
    cleaner code.

--- scripts/split-by-word.js
+++ scripts/split-by-word.js
@@ -1,5 +1,5 @@
 //split by word, so the animation can track when user has completed a word
-function splitByWord(){
+export default function splitByWord(){
     
     QUOTE.split(' ').forEach(character =>{
         console.log(character.length)
