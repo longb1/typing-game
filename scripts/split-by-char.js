2a5d481aa712c304154d816c678c621d777df8ea
Long <17272827+longb1@users.noreply.github.com>
2022-05-28 11:41:35 +0100

    return value in function
    split text by character to style it red/green later on, then return the
    changed array.

--- scripts/split-by-char.js
+++ scripts/split-by-char.js
@@ -1,10 +1,13 @@
 //split by character, for styling purposes
 export default function splitByChar(){
     
-    QUOTE.split('').forEach(character =>{
+    let charArray = QUOTE.split('')
+    
+    charArray.forEach(character =>{
         const characterSpan = document.createElement('span')
         characterSpan.innerText = character
         quoteDisplayElement.appendChild(characterSpan)
     })
     
+    return charArray
 }
