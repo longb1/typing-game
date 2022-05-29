38532389eb4491955bd91d504fac44ebe678a3ac
Long <17272827+longb1@users.noreply.github.com>
2022-05-29 10:23:32 +0100

    delegate logic to main.js
    move function contents to main.js to get base code working before
    exporting as module.

--- scripts/reset.js
+++ scripts/reset.js
@@ -1,8 +1,3 @@
 export default function reset(){
-    arrayIndex = 0;
-    currentWord='';
-    wordArray=[];
-    quoteDisplayElement.innerHTML=''
-    quoteInputElement.value = null
-    quoteInputElement.disabled="true";
+    
 }
